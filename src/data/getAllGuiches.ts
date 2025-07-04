bbcc4487a5dd334030abd507de8bd9bd0da4fd0f
Cleidimar Dias <cleidimarcdsc@gmail.com>
2025-07-04 13:13:22 -0300

    corrigido rota de guichê

--- src/data/getAllGuiches.ts
+++ src/data/getAllGuiches.ts
@@ -1,11 +1,11 @@
 export const getAllGuiches = async (reparticaoId: string) => {
   const res = await fetch(
     // `http://localhost:3001/guiche/reparticaoId/${reparticaoId}`
-    `${process.env.NEXT_PUBLIC_URL}guiche/reparticaoId/${reparticaoId}`
+    `${process.env.NEXT_PUBLIC_URL}/guiche/reparticaoId/${reparticaoId}`
   );
 
   if (!res.ok) {
-    throw new Error("Erro ao carregar serviços");
+    throw new Error("Erro ao carregar Guichês");
   }
 
   const data = await res.json();
